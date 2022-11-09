db694d9e3a0855ea0a2ec254fb2fffc715175824
flifalmed <flifalmed@gmail.com>
2022-11-09 12:27:05 +0100

    rating challenge

---
+++ main.js
@@ -0,0 +1,30 @@
+let btn = document.querySelectorAll(".numbers button");
+let out = document.getElementById("output");
+let sub = document.getElementById("submit");
+
+window.onload = function () {
+  sessionStorage.clear();
+};
+
+btn.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    btn.forEach((el) => {
+      el.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+
+    window.sessionStorage.setItem("rating", e.currentTarget.value);
+  });
+});
+document.addEventListener("click", function () {
+  out.textContent = window.sessionStorage.getItem("rating");
+});
+
+document.addEventListener("click", function () {
+  if (sessionStorage.getItem("rating") !== null) {
+    sub.addEventListener("click", function () {
+      document.getElementsByClassName("rating")[0].classList.add("del");
+      document.getElementsByClassName("thanks")[0].classList.add("act");
+    });
+  }
+});
