0e9623c23f4b65ec2b72c395d530020866ca64c3
flifalmed <flifalmed@gmail.com>
2023-05-08 14:28:01 +0100

    commited

--- main.js
+++ main.js
@@ -3,26 +3,26 @@ let out = document.getElementById("output");
 let sub = document.getElementById("submit");
 
 window.onload = function () {
-  sessionStorage.clear();
+    sessionStorage.clear();
 };
 
 btn.forEach((el) => {
-  el.addEventListener("click", (e) => {
-    btn.forEach((el) => {
-      el.classList.remove("active");
-    });
-    e.currentTarget.classList.add("active");
+    el.addEventListener("click", (e) => {
+        btn.forEach((el) => {
+            el.classList.remove("active");
+        });
+        e.currentTarget.classList.add("active");
 
-    window.sessionStorage.setItem("rating", e.currentTarget.value);
-	  out.textContent = window.sessionStorage.getItem("rating");
-  });
+        window.sessionStorage.setItem("rating", e.currentTarget.value);
+        out.textContent = window.sessionStorage.getItem("rating");
+    });
 });
 
 document.addEventListener("click", function () {
-  if (sessionStorage.getItem("rating") !== null) {
-    sub.addEventListener("click", function () {
-      document.getElementsByClassName("rating")[0].classList.add("del");
-      document.getElementsByClassName("thanks")[0].classList.add("act");
-    });
-  }
+    if (sessionStorage.getItem("rating") !== null) {
+        sub.addEventListener("click", function () {
+            document.getElementsByClassName("rating")[0].classList.add("del");
+            document.getElementsByClassName("thanks")[0].classList.add("act");
+        });
+    }
 });
